ea2fcb279e6619beaac98b50dfdfce3909132bfd
akolvakh <a.kolvakh@outlook.com>
2024-02-25 20:43:35 +0200

    GOIT-1 #comment fixes according to pr comments

--- task1.py
+++ task1.py
@@ -19,24 +19,31 @@ def generate_request():
         time.sleep(random.uniform(1, 3))
 
 # Функція для обробки заявки
-def process_request():
+def process_requests():
     start_time = time.time()
-    while time.time() - start_time < 15:
+    while time.time() - start_time < 45:  # Обмеження часу виконання до 45 секунд
         if not request_queue.empty():
             # Видаляємо заявку з черги
             request_id = request_queue.get()
             print(f"Заявка {request_id} обробляється")
             # Симуляція обробки заявки
             time.sleep(random.uniform(2, 5))
+            print(f"Заявка {request_id} видалена з черги")
         else:
             print("Черга порожня")
+            # Затримка перед наступною спробою взяти заявку з черги
+            time.sleep(1)
 
 # Запускаємо функцію для генерації заявок у окремому потоці
 generate_thread = threading.Thread(target=generate_request)
 generate_thread.start()
 
-# Запускаємо функцію для обробки заявок у головному потоці
-process_request()
+# Запускаємо функцію для обробки заявок у окремому потоці
+process_thread = threading.Thread(target=process_requests)
+process_thread.start()
 
 # Очікуємо завершення генерації заявок
 generate_thread.join()
+
+# Очікуємо завершення обробки заявок
+process_thread.join()
